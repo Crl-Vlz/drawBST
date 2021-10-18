89c2f694b243e9605a6a82f5ae8a4a6d4ebd9a4b
Crl-Vlz <crlvlz0215@gmail.com>
2021-10-18 17:25:03 -0500

    Quality of life changes for drawNode.py

--- drawNode.py
+++ drawNode.py
@@ -1,11 +1,11 @@
-import pygame, sys
+import pygame
 
 class drawNode:
-    def __init__(self, value, color, center, radius):
+    def __init__(self, value, color, radius):
         value = str(value)
         self.value = value
         screen = pygame.Surface
-        pygame.draw.circle(screen, color, center, radius)
+        pygame.draw.circle(screen, color, (radius/2, radius/2), radius)
         pygame.font.init()
         textImg = pygame.render(value, False, (000, 000, 000))
         screen.blit(textImg)
