3b1707b40651c8231b7cb1a072b4428a24682ec4
ximemunoz <92742110+ximemunoz@users.noreply.github.com>
2021-10-20 10:31:24 -0500

    commit

--- drawNode.py
+++ drawNode.py
@@ -1,19 +1,13 @@
-import pygame
+import pygame, sys
 
-# Returns a pygame Surface that can then be drawn to show an individual node
-# this does not include the arysts
 class drawNode:
-    def __init__(self):
-        pass
-    def draw(self, value, color, radius, f):
+    def __init__(self, value, color, center, radius):
         value = str(value)
         self.value = value
-        screen = pygame.Surface((radius*2, radius*2))
-        screen.fill((255, 255, 255, 0))
-        pygame.draw.circle(screen, color, (radius, radius), radius)
+        screen = pygame.Surface
+        pygame.draw.circle(screen, color, center, radius)
         pygame.font.init()
-        font = pygame.font.SysFont(None, f)
-        textImg = font.render(value, False, (000, 000, 000))
-        pygame.Surface.blit(screen, textImg, (radius, radius))
+        textImg = pygame.render(value, False, (000, 000, 000))
+        screen.blit(textImg)
         pygame.font.quit()
         return screen
